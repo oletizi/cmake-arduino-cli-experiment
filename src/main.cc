c3512d42e8d0967c3e94f0bec23ff30badd7175d
orion <oletizi@mac.com>
2023-02-11 18:49:05 -0800

    - added juce target_link_libraries

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
 #include <iostream>
-int main() {
+#include <juce_core/juce_core.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+
+int main(int argc, char *argv[]) {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
-
